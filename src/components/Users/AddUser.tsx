7c73bc1f907638c0ac3c50314dc83ed1e2183214
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-10 13:25:08 +0300

    feat(App): add basic user input mechanics

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,19 +1,36 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 
 const AddUser: FC = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState<string>("");
+  const [enteredUserage, setEnteredUserage] = useState<string>("");
+
   const addUserHandler: React.FormEventHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredUserage);
   };
+
+  const usernameChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const userageChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setEnteredUserage(event.target.value);
+  };
+
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="userName">Username</label>
-        <input id="userName" type="text" />
+        <input id="userName" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="userAge">Age (Years)</label>
-        <input id="userAge" type="number" />
+        <input id="userAge" type="number" onChange={userageChangeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
