dbcde84d8ad552ef628ff86fd76df44e06930629
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-10 15:01:26 +0300

    feat(App): implement state lifting when adding a user

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 import { AppUser } from "./models/appUser";
 
 import "./index.css";
+import { AddUserEventHandler } from "./models/eventHandlers";
 
 function App() {
+  const [users, setUsers] = useState<AppUser[]>([]);
+  const [isEmpty, setIsEmpty] = useState<boolean>(true);
+
+  const addUserHandler: AddUserEventHandler = (user) => {
+    setUsers((prevState) => {
+      const newUsersList = [...prevState, user];
+      if (newUsersList.length > 0) setIsEmpty(false);
+      return newUsersList;
+    });
+  };
+
   return (
     <>
-      <AddUser />
-      <UsersList users={[new AppUser('John', 31)]} />
+      <AddUser onAddUser={addUserHandler} />
+      {!isEmpty && <UsersList users={users} />}
     </>
   );
 }
