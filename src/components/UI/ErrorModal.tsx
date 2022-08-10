e9921430bf38607e07b674ed8289176fbd477768
Artur Abdullin <artur.abdullin@gmail.com>
2022-08-10 20:43:05 +0300

    feat(ErrorModal): use React Portals to resolve semantic issues

--- src/components/UI/ErrorModal.tsx
+++ src/components/UI/ErrorModal.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import RDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
@@ -9,21 +10,43 @@ type ErrorModalProps = {
   onConfirm: () => void;
 };
 
+type BackdropProps = Pick<ErrorModalProps, "onConfirm">;
+
+const Backdrop: FC<BackdropProps> = (props) => (
+  <div className={styles.backdrop} onClick={props.onConfirm}></div>
+);
+
+const ModalOverlay: FC<ErrorModalProps> = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal: FC<ErrorModalProps> = (props) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={props.onConfirm}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onConfirm}>Ok</Button>
-        </footer>
-      </Card>
+      {RDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root") as HTMLElement
+      )}
+      {RDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root") as HTMLElement
+      )}
     </>
   );
 };
